2f80b5e828bb8b3c771b3e4f5c7ed532f9725fd7
Anthony Ettinger <anthony@strongloop.com>
2014-10-15 11:20:05 -0700

    Add error handling to forms
    - set valid and invalid states on form fields
    - add error message below form fields

--- client/www/scripts/modules/build-deploy/build-deploy.controllers.js
+++ client/www/scripts/modules/build-deploy/build-deploy.controllers.js
@@ -8,6 +8,40 @@ BuildDeploy.controller('BuildDeployController', [
     $scope.buildId = 'git';
     $scope.deployId = 'new';
 
+    $scope.build = {
+      git: {
+        url: '',
+        deploy: ''
+      },
+      universal: {
+        archive: ''
+      }
+    };
+
+    $scope.deploy = {
+      git: {
+        url: '',
+        deploy: ''
+      },
+      universal: {
+        git: '',
+        archive: ''
+      },
+      host: { hostname: '', port: '', processes: '' }
+    };
+
+    $scope.$watch('build.git.url', function(newVal){
+      $scope.deploy.git.url = newVal;
+    });
+
+    $scope.$watch('build.git.deploy', function(newVal){
+      $scope.deploy.git.deploy = newVal;
+    });
+
+    $scope.$watch('build.universal.archive', function(newVal){
+      $scope.deploy.universal.archive = newVal;
+    });
+
     $scope.showGit = function(){
       $log.log('clicked showGit()');
     };
@@ -16,11 +50,13 @@ BuildDeploy.controller('BuildDeployController', [
       $log.log('clicked showUniversal()');
     };
 
+    //todo click events may not be needed here
     $scope.buildTogglers = [
       { id: 'git', label: 'Git', click: $scope.showGit, activeId: 'buildId' },
       { id: 'universal', label: 'Universal', click: $scope.showUniversal, activeId: 'buildId' }
     ];
 
+    //todo click events may not be needed here
     $scope.deployTogglers = [
       { id: 'new', label: 'New', click: $scope.showNew, activeId: 'deployId' },
       { id: 'existing', label: 'Existing', click: $scope.showExisting, activeId: 'deployId' }
@@ -31,6 +67,27 @@ BuildDeploy.controller('BuildDeployController', [
     $scope.clickUploadFile = function(e){
       $('[type=file]').click();
     };
+
+    $scope.buildGit = function(form){
+      $scope.build.git.submitted = true;
+      $log.log(form);
+    };
+
+    $scope.buildUniversal = function(form){
+      $scope.build.universal.submitted = true;
+      $log.log(form);
+    };
+
+    $scope.deployGit = function(form){
+      $scope.deploy.git.submitted = true;
+      $log.log(form);
+    };
+
+    $scope.deployUniversal = function(form){
+      $scope.deploy.universal.submitted = true;
+      $log.log(form);
+    };
+
 
     BuildDeployService.doSomething()
       .then(function (data) {
